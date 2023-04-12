eea41b67f332341d0c0b7ad59a412d2b0e995134
Utkarsh Jaiswal <utkarshjaiswal2580@gmail.com>
2023-04-12 21:29:13 +0530

    Initial commit

---
+++ src/main/java/com/utkarsh/fextnextnumber/FextNextNumberApplication.java
@@ -0,0 +1,13 @@
+package com.utkarsh.fextnextnumber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FextNextNumberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FextNextNumberApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/utkarsh/fextnextnumber/FextNextNumberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utkarsh.fextnextnumber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FextNextNumberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
